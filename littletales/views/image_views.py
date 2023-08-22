15e6237245a246d2154724ccdec83234937b9dfb
Zzanzzan <crkim1228@naver.com>
2023-08-22 14:57:36 +0900

    Feat : 서버 환경에서 작동되는 llama2 모델을 로컬 환경에서 작동되도록 gpt3.5turbo 모델로 변경

--- littletales/views/image_views.py
+++ littletales/views/image_views.py
@@ -4,11 +4,11 @@ from flask import Blueprint
 
 bp = Blueprint('littleimage', __name__, url_prefix='/littleimage')
 
-pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
-pipe.to("cuda")
-
-prompt1 = "a smiling puppy"
-prompt2 = "a smiling kitten"
-
-images = pipe(prompt=prompt1).images[0]
-images2 = pipe(prompt=prompt2).images[0]
+# pipe = DiffusionPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, use_safetensors=True, variant="fp16")
+# pipe.to("cuda")
+#
+# prompt1 = "a smiling puppy"
+# prompt2 = "a smiling kitten"
+#
+# images = pipe(prompt=prompt1).images[0]
+# images2 = pipe(prompt=prompt2).images[0]
--- littletales/views/read_views.py
+++ littletales/views/read_views.py
@@ -19,31 +19,31 @@ dotenv.load_dotenv(dotenv_file)
 
 key = os.environ["OPENAI_API_KEY"]
 
-# deepl인증키 및 메소드 가져오기
-deepl_key = "d7ff04a7-09eb-0c77-558a-27575e0361d4:fx"
-translator = deepl.Translator(deepl_key)
-
-# 허깅페이스 인증키 가져오기
-# 터미널에서 허깅페이스 로그인 선작업 해야함
-# huggingface-cli login
-hugging_face_key ="hf_wxwkJyjXaHNbvtwEfwFZqDdQEiTFJSuiFAD"
-hf_token = os.environ.get(hugging_face_key)
-api = HfApi()
-api.token = hf_token
-
-model_llama2 = "meta-llama/Llama-2-7b-chat-hf"
-
-tokenizer = AutoTokenizer.from_pretrained(
-    model_llama2,
-    use_auth_token=True,
-)
-
-pipeline = transformers.pipeline(
-    "text-generation",
-    model=model_llama2,
-    torch_dtype=torch.float16,
-    device_map="auto",
-)
+# # deepl인증키 및 메소드 가져오기
+# deepl_key = "d7ff04a7-09eb-0c77-558a-27575e0361d4:fx"
+# translator = deepl.Translator(deepl_key)
+#
+# # 허깅페이스 인증키 가져오기
+# # 터미널에서 허깅페이스 로그인 선작업 해야함
+# # huggingface-cli login
+# hugging_face_key ="hf_wxwkJyjXaHNbvtwEfwFZqDdQEiTFJSuiFAD"
+# hf_token = os.environ.get(hugging_face_key)
+# api = HfApi()
+# api.token = hf_token
+#
+# model_llama2 = "meta-llama/Llama-2-7b-chat-hf"
+#
+# tokenizer = AutoTokenizer.from_pretrained(
+#     model_llama2,
+#     use_auth_token=True,
+# )
+#
+# pipeline = transformers.pipeline(
+#     "text-generation",
+#     model=model_llama2,
+#     torch_dtype=torch.float16,
+#     device_map="auto",
+# )
 
 @bp.route('/', methods=['POST'])
 def read_index() :
@@ -53,7 +53,8 @@ def read_index() :
     chat_response = None
 
     if animal_name:
-        chat_response = make_llama(animal_name)
+        # chat_response = make_llama(animal_name)
+        chat_response = make_gpt(animal_name)
 
     print('생성 동화 : ', chat_response)
     return render_template('little_read.html', animal_name=animal_name, chat_response=chat_response)
@@ -91,40 +92,40 @@ def make_gpt(animal_name):
 
     return result
 
-def make_llama(animal_name) :
-    print('동물 : ', animal_name)
-
-    while True :
-        answer = gen2(
-            f"Please make a fairy tale with the main character {animal_name} for the children's audience. Be creative and don't worry, and make a great fictional story for children",
-            1500)
-        print('answer : ', answer)
-        if len(str(answer)) > 200 :
-            break
-
-    result = translator.translate_text(answer, target_lang="ko")
-    result = str(result).strip().split("\n")
-
-    result_str = ""
-    for i in result:
-        if (len(i) > 4):
-            # print(i)
-            result_str += i + "\n"
-
-    result_str += '-끝-'
-
-    print('답변 : ', result_str)
-
-    return result_str
-
-def gen2(x, max_length):
-    sequences = pipeline(
-        x,
-        do_sample=True,
-        top_k=10,
-        num_return_sequences=1,
-        eos_token_id=tokenizer.eos_token_id,
-        max_length=max_length,
-    )
-
-    return sequences[0]["generated_text"].replace(x, "")
+# def make_llama(animal_name) :
+#     print('동물 : ', animal_name)
+#
+#     while True :
+#         answer = gen2(
+#             f"Please make a fairy tale with the main character {animal_name} for the children's audience. Be creative and don't worry, and make a great fictional story for children",
+#             1500)
+#         print('answer : ', answer)
+#         if len(str(answer)) > 200 :
+#             break
+#
+#     result = translator.translate_text(answer, target_lang="ko")
+#     result = str(result).strip().split("\n")
+#
+#     result_str = ""
+#     for i in result:
+#         if (len(i) > 4):
+#             # print(i)
+#             result_str += i + "\n"
+#
+#     result_str += '-끝-'
+#
+#     print('답변 : ', result_str)
+#
+#     return result_str
+#
+# def gen2(x, max_length):
+#     sequences = pipeline(
+#         x,
+#         do_sample=True,
+#         top_k=10,
+#         num_return_sequences=1,
+#         eos_token_id=tokenizer.eos_token_id,
+#         max_length=max_length,
+#     )
+#
+#     return sequences[0]["generated_text"].replace(x, "")
