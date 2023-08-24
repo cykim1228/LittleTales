8df89b6e29c76cc58a12f5197fc20ac81269470d
Zzanzzan <crkim1228@naver.com>
2023-08-24 23:51:59 +0900

    Feat : 그림 그리기 전 인풋 화면 추가

--- littletales/views/draw_views.py
+++ littletales/views/draw_views.py
@@ -4,7 +4,13 @@ bp = Blueprint('littledraw', __name__, url_prefix='/littledraw')
 
 @bp.route('/')
 def draw_index() :
-    animal_name = request.args.get('animal')
+
+    return render_template('little_input.html')
+
+@bp.route('/canvas', methods=['POST'])
+def draw_canvas() :
+    animal_name = request.form.get('animal')
     print('판별된 동물 : ', animal_name)
 
     return render_template('little_draw.html', animal_name=animal_name)
+
