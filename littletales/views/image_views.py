dabcb10ce1921a92a5972e6621a2010da5161008
littleTales <choinoonsol1@naver.com>
2023-08-27 17:14:56 +0900

    DALL_E2로 변경해서 논문 참조해서 코드 적용

--- littletales/views/image_views.py
+++ littletales/views/image_views.py
@@ -1,74 +1,61 @@
-#이미지 불러오기
-#
-# 누끼 따기
-#
-# 이미지 저장
-#
-# dall-e 2써서 프롬프트
-#
-# 이미지 생성
-
-
 from PIL import Image
 import requests
 import os
 import openai
 import dotenv
+from rembg import remove
+from transformers import GPT2Tokenizer
 import json
-from PIL import Image
 
-# from flask import Blueprint
-# from matplotlib import pyplot as plt
-# from rembg import remove
+#플라스크 부분은 주석처리 해 둠
+#from flask import Blueprint
+#from matplotlib import pyplot as plt
 # openai.organization = "org-9wDldVZ1upcx03krVviK4tWw"
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-# openai.Model.list()
+#bp = Blueprint('littleimage', __name__, url_prefix='/littleimage')
 
-dotenv_file = dotenv.find_dotenv()
-dotenv.load_dotenv(dotenv_file)
-
-openai.organization = "org-jUejdT5GvU7fyKtp3IAwwin9"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
-
-##key file 값 불러오기
-dotenv_file = dotenv.find_dotenv()
-dotenv.load_dotenv(dotenv_file)
-key = os.environ["OPENAI_API_KEY"]
-openai.api_key = key
-##
-#PIL저장
-from PIL import Image
-
-
-# 라이브러리 임포트
+# key file 값 불러오기 (환경변수 로딩)
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
 # API 키 설정
+openai.organization = "org-jUejdT5GvU7fyKtp3IAwwin9"
 openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.Model.list()
 
-#bp = Blueprint('littleimage', __name__, url_prefix='/littleimage')
+# 위 코드랑 중복되는 거라 일단 둘 중 뭐가 맞는 지 헷갈려서 주석처리
+# key = os.environ["OPENAI_API_KEY"]
+# openai.api_key = key
 
 # 이미지 불러오기
 image_path = "D:/projects/littletales/littletales/uploads/dog.png"
 image = Image.open(image_path)
-print("이미지 불러오기")
+print("이미지 불러오기 완료")
 
 # 배경 제거
 output = remove(image)
-print("배경 제거")
+print("배경 제거 완료")
 
 # 이미지 저장 (rembg.png 로 저장)
 output.save('rembg.png')
-print("이미지 저장")
+print("배경제거된 이미지 저장 완료")
+
+# GPT-2 토크나이저 로드
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 # 프롬프트 입력
 prompt = input("depict the background as a fairy tale with the characteristics of the painting: ")
-#
-# # 프롬프트를 토큰화
+
+# 프롬프트를 토큰화_방법1
 #inputs = tokenizer(prompt, return_tensors="pt")
-#print("프롬프트")
+# 프롬프트를 토큰화_방법2
+tokens = tokenizer.tokenize(prompt)
+token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+print("프롬프트 입력 완료")
+
+# 굳이 안필요할거같아서 주석처리
+# print("토큰화된 프롬프트:", tokens)
+# print("토큰 ID:", token_ids)
 
 # 이미지 생성
 response = openai.Image.create_edit(
@@ -80,6 +67,8 @@ response = openai.Image.create_edit(
 )
 print("이미지 생성")
 
+image_url = response['data'][0]['url']
+
 # 생성된 이미지 플롯
 # image = response.edits[0].image
 # image.save('result.png')
@@ -89,67 +78,12 @@ print("이미지 생성")
 #
 # with torch.no_grad():
 #     outputs = model.generate_images(**inputs)
-#
-# import matplotlib.pyplot as plt
 
-
-image_url = response['data'][0]['url']
-
-##생성된 이미지 url값을 가져온다
-im = Image.open(requests.get(image_url, stream=True).raw)
-im.show()
-
-
-
-
-# from PIL import Image
-# import requests
-# import os
-# import openai
-# import dotenv
-# import json
-# from PIL import Image
-#
+# 생성된 이미지 url값을 가져온다
+# im = Image.open(requests.get(image_url, stream=True).raw)
+# im.show()
 # ##key file 값 불러오기
 # dotenv_file = dotenv.find_dotenv()
 # dotenv.load_dotenv(dotenv_file)
 # key = os.environ["OPENAI_API_KEY"]
 # openai.api_key = key
-# ##
-#
-# ##image generation
-# response = openai.Image.create(
-#   prompt="a white siamese cat",
-#   n=1,
-#   size="1024x1024"
-# )
-# image_url = response['data'][0]['url']
-#
-# ##생성된 이미지 url값을 가져온다
-# im = Image.open(requests.get(image_url, stream=True).raw)
-# im.show()
-#
-# #PIL저장
-# from PIL import Image
-#
-# # 이미지를 불러오고 처리하는 등의 코드가 있겠죠?
-# img = Image.open('imagepath+file')
-#
-# # 저장해봅시다.
-# img.save('sample.png', 'png')
-#
-# # 특정 폴더에 저장하려면
-# # 저장하고자 하는 폴더의 절대경로를 입력해주면 됩니다.
-# # 경로 앞에 / 가 들어가줘야된다는걸 기억!!
-# img.save('/content/data/output.png', 'png')
-#
-# #경우에 따라 상대경로를 입력해도 됩니다.
-# img.save('./data/output.png', 'png')
-#
-#
-#
-# # 이미지를 엽니다.
-# img = Image.open('example.jpg')
-#
-# # 다른 형식으로 이미지를 저장합니다.
-# img.save('example.png', 'PNG')
